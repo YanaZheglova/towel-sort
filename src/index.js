8a1e824e34e1d0839f530a23127fe293d133c208
Yana <you@example.com>
2023-01-23 22:30:10 +0300

    add: towel

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 
 // You should implement your task here.
+//нечетные подмассивы по возрастанию, а четные - по убыванию нечетные подмассивы по возрастанию, а четные - по убыванию 
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+    if (!matrix) return []
+
+    return matrix.reduce((accumulator, element, index) => {
+        if (index % 2 == 0) accumulator.push(...element)
+        else accumulator.push(...element.reverse())
+        return accumulator
+    }, [])
+};
